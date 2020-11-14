2efe5fcd365855e43b5e2522b3964a9351b0df92
girish5f9@sasi.ac.in <girish5f9@sasi.ac.in>
2020-11-14 08:50:21 +0530

    Updates

---
+++ precache-manifest.029de609dae30cb85b787181bc529f90.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eb8bcf28a8ab63cc09fddb4c32d441",
+    "url": "/profile1/index.html"
+  },
+  {
+    "revision": "fcc156f90a223711b4d9",
+    "url": "/profile1/static/css/main.bb02298f.chunk.css"
+  },
+  {
+    "revision": "e8fd62bd97c2b6c428ee",
+    "url": "/profile1/static/js/2.1fb75f50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile1/static/js/2.1fb75f50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcc156f90a223711b4d9",
+    "url": "/profile1/static/js/main.5c5e5521.chunk.js"
+  },
+  {
+    "revision": "cef9acf5d3c9f20eb0a0",
+    "url": "/profile1/static/js/runtime-main.bd06f898.js"
+  },
+  {
+    "revision": "48b778f8aa7dffa90d0a4af237feb40f",
+    "url": "/profile1/static/media/bg.48b778f8.jpeg"
+  },
+  {
+    "revision": "8c1edd9afbdbc6b8a30b72bc3588d50c",
+    "url": "/profile1/static/media/bg1.8c1edd9a.png"
+  },
+  {
+    "revision": "6083b52c455f98792736a5f702d573c6",
+    "url": "/profile1/static/media/logo.6083b52c.png"
+  }
+]);
